8aeaf9fa9264ef6cd0b9abb6615dae9c987ddc26
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-05-01 17:12:47 +0900

    Update pyMegaboxChart.py

--- pyMegaboxChart.py
+++ pyMegaboxChart.py
@@ -12,7 +12,7 @@ import json
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
-filename = f"pyMegaboxChart_{current_date}.json"
+filename = f"Megabox/MegaboxChart_{current_date}.json"
 
 
 # 웹드라이브 설치
